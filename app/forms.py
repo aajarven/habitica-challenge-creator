9d446d07bf5dbb4f9f53892561969ffb7b3c3f29
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-01-22 14:59:06 +0000

    Linting

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, UUID
 from wtforms.widgets import TextArea
 
 
-class ChallengeForm(FlaskForm):
+class ChallengeForm(FlaskForm):  # pylint: disable=too-few-public-methods
     """
     A form for submitting challenge information.
     """
@@ -16,7 +16,8 @@ class ChallengeForm(FlaskForm):
         DataRequired("Field cannot be empty"),
         UUID("This does not look like a valid API key"),
         ])
-    challenge_data = StringField("Challenge data", widget=TextArea(),
+    challenge_data = StringField(
+            "Challenge data", widget=TextArea(),
             validators=[DataRequired("Field cannot be empty")],
             render_kw={"rows": 50, "cols": 120})
     create_challenge = SubmitField("Create a new challenge")
