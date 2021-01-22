938ec12ebf8c50be88dcb4f34e6220097249ca38
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-01-22 14:58:04 +0000

    Show challenge name when form is submitted

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,10 @@ Routes for the web app
 """
 
 from flask import render_template, flash
-import time
 
 from app import app
 from app.forms import ChallengeForm
+from functionality.challenge_creator import ChallengeCreator
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -18,5 +18,7 @@ def index():
     if challenge_form.validate_on_submit():
         flash("Got challenge creation request from {}"
               "".format(challenge_form.habitica_id.data))
-        time.sleep(4)
+        challenge_creator = ChallengeCreator(
+                challenge_form.challenge_data.data)
+        flash(challenge_creator.name)
     return render_template("index.html", form=challenge_form)
