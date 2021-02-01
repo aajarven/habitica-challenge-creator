d8edae47dff9bf477091ddcf88e2e6bccd735952
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-02-01 19:01:38 +0000

    Base class for task parsing

---
+++ functionality/task_parser.py
@@ -0,0 +1,31 @@
+"""
+Extract the task data from user input.
+"""
+
+
+class TaskParser():
+    """
+    Base parser that can just parse task type and name of the task.
+    """
+
+    def __init__(self, task_str):
+        """
+        Create the parser.
+
+        :task_str: String representing the task.
+        """
+        self._task_str = task_str
+
+    def task_type(self):
+        """
+        Return the task type.
+        """
+        return self._task_str.split(";")[0]
+        # TODO: validation, tests
+
+    def name(self):
+        """
+        Return the name of the task.
+        """
+        return self._task_str.split(";")[1]
+        # TODO: validation, tests
